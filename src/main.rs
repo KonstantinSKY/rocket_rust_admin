e649909d9d106b3e2e3c7bd907dee8f9134b4754
SKY <sky012877@gmail.com>
2024-06-10 20:04:08 -0700

    src/main.rs - Modified :

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
 #[macro_use] extern crate rocket;
 
-use rocket::serde::json::{Value, json};
+use routes::{hello, hi_json};
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world, Hey!"
-}
+mod routes;
+mod api;
+mod models;
+mod services;
 
-#[get("/")]
-fn hi_json() -> Value {
-    json!("{key: Hello, world, JSON!}")
-}
-
-// #[rocket::main]
-// async fn main() {
-//     let _ = rocket::build()
-//         .mount("/", routes![hello])
-//         .launch()
-//         .await;
-// }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![hello])
-        .mount("/hi_json", routes![hi_json])
+        .mount("/", routes![hello::hello])
+        .mount("/hi_json", routes![hi_json::hi_json])
 }
