1d738f80207781477620760a59a4f3170f4de0e0
SKY <sky012877@gmail.com>
2024-06-11 14:35:11 -0700

    src/settings.rs - Modified :

--- src/settings.rs
+++ src/settings.rs
@@ -1,10 +1,20 @@
-use rocket::routes;
+// use rocket::routes;
+use rocket::Route;
 
+// // use crate::routes::hello::hello;
+// use crate::routes::hi_json;
 use crate::routes::{hello, hi_json};
 
-pub fn get_routes() -> Vec<rocket::Route> {
+// Routers settings
+// pub const ROUTE_HANDLERS: &[fn() -> Route] = &[
+//     hello::hello,
+//     hi_json::hi_json,
+// ];
+
+// routes 
+pub fn get_routes() -> Vec<Route> {
     routes![
+        hi_json::hi_json,
         hello::hello,
-        hi_json::hi_json
-    ]
+        ]
 }
