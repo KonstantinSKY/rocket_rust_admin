6095a6734b70ddf7497bb4b439c6e373319272db
SKY <sky012877@gmail.com>
2024-06-10 18:39:52 -0700

    src/main.rs - Modified :

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world, Hey!"
+}
+
+// #[rocket::main]
+// async fn main() {
+//     let _ = rocket::build()
+//         .mount("/", routes![hello])
+//         .launch()
+//         .await;
+// }
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![hello])
 }
