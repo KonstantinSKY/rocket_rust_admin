6360fb3b01021edf035639d67430b47f7f287a90
SKY <sky012877@gmail.com>
2024-06-12 12:41:38 -0700

    src/api/open_api.rs - Modified : added Swagger and Rapidoc to app

--- src/api/open_api.rs
+++ src/api/open_api.rs
@@ -3,7 +3,7 @@
 // use rocket_okapi::openapi_get_routes;
 // use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 // use rocket_okapi::rapidoc::{make_rapidoc, RapiDocConfig};
-// // use crate::routes::handlers::ROUTE_HANDLERS;
+// use crate::routes::handlers::ROUTE_HANDLERS;
 // // use crate::routes::hello::hello;
 // use crate::routes::hi_json::hi_json;
 // use crate::routes::hi_json::okapi_add_operation_for_hi_json_;
@@ -19,16 +19,7 @@
 //     routes.append(&mut make_rapidoc(&get_rapidoc_config()));
     
 //     routes
-//}
-// #[get("/openapi.json")]
-// pub fn openapi_json() -> Json<String> {
-//     let settings = OpenApiSettings::default();
-//     let mut gen = OpenApiGenerator::new(&settings);
-//     let routes = openapi_get_routes![hi_json];
-//     let spec = gen.merge(routes);
-//     Json(spec.to_pretty_json().unwrap())
 // }
-
 
 // pub fn get_open_api() -> Vec<Route> {
 //     let mut routes = openapi_get_routes![hi_json];
@@ -37,12 +28,7 @@
 //     routes
 // }
 
-// pub fn swagger_ui_routes() -> Vec<Route> {
-//     make_swagger_ui(&SwaggerUIConfig {
-//         url: "../openapi.json".to_owned(),
-//         ..Default::default()
-//     }).into()
-// }
+
 
 use rocket::Route;
 use rocket_okapi::{
@@ -50,11 +36,28 @@ use rocket_okapi::{
         make_rapidoc, GeneralConfig, HideShowConfig, RapiDocConfig
     }, 
     settings::UrlObject, swagger_ui::{
-        make_swagger_ui, SwaggerUIConfig}
-    };
+        make_swagger_ui, SwaggerUIConfig
+    ,}
+};
+use crate::routes::{
+    hello::hello, hello::okapi_add_operation_for_hello_,
+    hi_json::hi_json, hi_json::okapi_add_operation_for_hi_json_,
+};
 
-// Function to create routes for RapiDoc
-pub fn rapidoc_routes() -> Vec<Route> {
+
+pub fn get_open_api_routes() -> Vec<Route> {
+    openapi_get_routes![hi_json, hello]
+    }
+
+pub fn get_swagger_routes() -> Vec<Route> {
+    make_swagger_ui(&SwaggerUIConfig {
+        url: "../openapi.json".to_owned(),
+            ..Default::default()
+        }).into()
+    }
+
+    // Function to create routes for RapiDoc
+pub fn get_rapidoc_routes() -> Vec<Route> {
     make_rapidoc(&RapiDocConfig {
         general: GeneralConfig {
             spec_urls: vec![UrlObject::new("General", "../openapi.json")],
