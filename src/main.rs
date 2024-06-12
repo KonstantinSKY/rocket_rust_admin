2abe870988c68b47466cf6d53f5b9fb90a4cd179
SKY <sky012877@gmail.com>
2024-06-12 13:30:20 -0700

    src/main.rs - Modified :

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-
 #[macro_use] extern crate rocket;
+use dotenv::dotenv;
 
 mod db;
 mod routes;
@@ -10,12 +10,14 @@ mod settings;
 
 #[rocket::main]
 async fn main() {
+    dotenv().ok();                                          // Load environment variables
+
     let _ = rocket::build()
-        .attach(db::stage()) 
-        .mount("/", settings::get_routes())
-        .mount("/doc", api::get_open_api_routes())
-        .mount("/doc/swagger/", api::get_swagger_routes())
-        .mount("/doc/rapidoc/", api::get_rapidoc_routes())
+        .attach(db::stage())                               // Attach your database stage
+        .mount("/", settings::get_routes())                // Mount your application routes
+        .mount("/doc", api::get_open_api_routes())         // Mount OpenAPI routes
+        .mount("/doc/swagger/", api::get_swagger_routes()) // Mount Swagger UI routes
+        .mount("/doc/rapidoc/", api::get_rapidoc_routes()) // Mount RapiDoc routes
         .launch()
         .await;
 }
