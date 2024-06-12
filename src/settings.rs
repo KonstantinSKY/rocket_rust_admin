74195124db2c01c96b73ab3b4f6c9a6d9ce4f065
SKY <sky012877@gmail.com>
2024-06-12 14:37:44 -0700

    src/settings.rs - Modified :

--- src/settings.rs
+++ src/settings.rs
@@ -1,13 +1,39 @@
-use rocket::Route;
+use std::env;
+
+use dotenv::dotenv;
+use rocket::{figment::Figment, Config, Route};
 
 
 use crate::routes::{hello, hi_json};
 
+pub struct Settings {
+    pub secret_key: String,
+    pub figment: Figment,
+    pub routes: Vec<Route>,
+}
+
+impl Settings {
+    pub fn new () -> Self {
+        dotenv().ok(); // Load environment variables
+        let secret_key = env::var("SECRET_KEY").expect("SECRET_KEY must be set");
+        let figment = Config::figment()   
+                                .merge(("secret_key", secret_key.clone()));
+        
+        Self {
+            secret_key,
+            figment,
+            routes: get_routes(),
+        }        
+    }
+    
+    
+}
 
 //  === Routes 
-pub fn get_routes() -> Vec<Route> {
+fn get_routes() -> Vec<Route> {
     routes![
         hi_json::hi_json,
         hello::hello,
         ]
 }
+
