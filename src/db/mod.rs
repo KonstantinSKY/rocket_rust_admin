3ae59eca9d45a68389c730a3ab189b6d20d4cb83
SKY <sky012877@gmail.com>
2024-06-12 23:40:42 -0700

    src/db/mod.rs - Modified :

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,14 +3,13 @@ use sea_orm::{DatabaseConnection, DbErr};
 use rocket::fairing::AdHoc;
 
 pub async fn init_database() -> Result<DatabaseConnection, DbErr> {
-    let db = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
+    let db: String = env::var("POSTGRES_DB").expect("POSTGRES_DB must be set");
     let password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD must be set");
     let host = env::var("POSTGRES_HOST").expect("POSTGRES_DB must be set");
     let port  = env::var("POSTGRES_PORT").expect("POSTGRES_DB must be set");
     let username  = env::var("POSTGRES_USERNAME").expect("POSTGRES_DB must be set");
 
-    let database_url = format!("postgresql://{username}:{password}@{host}:{port}/{db}");
-    println!("Data Base URL: {}", database_url);
+    let database_url = format!("postgres://{username}:{password}@{host}:{port}/{db}");
 
     sea_orm::Database::connect(&database_url).await
 }
@@ -18,9 +17,12 @@ pub async fn init_database() -> Result<DatabaseConnection, DbErr> {
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("Database Stage", |rocket| async {
         match init_database().await {
-            Ok(database) => rocket.manage(database),
+            Ok(database) => {
+                println!("Data Base Connected!");
+                rocket.manage(database)},
             Err(e) => {
                 println!("Database connection failed: {}", e);
+                // panic!();
                 rocket
             },
         }
