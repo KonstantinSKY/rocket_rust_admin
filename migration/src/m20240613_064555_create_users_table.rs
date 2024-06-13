35213e2182cf3c1e1a7c358f65d069774d69183a
SKY <sky012877@gmail.com>
2024-06-13 00:36:03 -0700

    migration/src/m20240613_064555_create_users_table.rs - Added :

---
+++ migration/src/m20240613_064555_create_users_table.rs
@@ -0,0 +1,46 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.create_table(
+            Table::create()
+                .table(User::Table)
+                .if_not_exists()
+                .col(
+                    ColumnDef::new(User::Id)
+                        .integer()
+                        .not_null()
+                        .auto_increment()
+                        .primary_key(),
+                )
+                .col(ColumnDef::new(User::Name).string().not_null().unique_key())
+                .col(ColumnDef::new(User::Email).string().not_null().unique_key())
+                .col(ColumnDef::new(User::FirstName).string())
+                .col(ColumnDef::new(User::LastName).string())
+                .col(ColumnDef::new(User::CreatedAt)
+                    .timestamp()
+                    .not_null()
+                    .default(Expr::current_timestamp()))
+                .to_owned(),
+        ).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.drop_table(Table::drop().table(User::Table).to_owned()).await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Email,
+    FirstName,
+    LastName,
+    CreatedAt,
+}
