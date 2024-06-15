c6a92d43c8ca0a3608c5cf4091d99f51004938d7
SKY <sky012877@gmail.com>
2024-06-14 19:10:18 -0700

    src/routes/hello.rs - Modified :

--- src/routes/hello.rs
+++ src/routes/hello.rs
@@ -2,9 +2,16 @@
 use rocket_okapi::openapi;
 
 
-/// First api Hello world for test
+/// First api Hello world for testi
+
 #[openapi]
 #[get("/hello")]
 pub fn hello() -> &'static str {
     "Hello, world!"
 }
+
+// #[openapi]
+// #[get("/doc/hello")]
+// pub fn hello_openapi() -> &'static str {
+//     "Hello, world!"
+// }
