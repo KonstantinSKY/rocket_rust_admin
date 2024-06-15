90c35f454d2a53b9937977ea42cb1e400202b453
SKY <sky012877@gmail.com>
2024-06-14 19:10:18 -0700

    migration/src/lib.rs - Modified :

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,8 @@
 pub use sea_orm_migration::prelude::*;
 mod field_types;
 mod m20240613_064555_create_users_table;
+mod m20240614_173112_create_group_table;
+mod m20240614_180504_create_user_group_table;
 
 pub struct Migrator;
 
@@ -9,6 +11,8 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20240613_064555_create_users_table::Migration),
+            Box::new(m20240614_173112_create_group_table::Migration),
+            Box::new(m20240614_180504_create_user_group_table::Migration),
         ]
     }
 }
