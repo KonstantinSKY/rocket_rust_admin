722d0a4630a6cb0ee2ff1efe87aae5b73a6586a1
SKY <sky012877@gmail.com>
2024-06-14 19:10:18 -0700

    src/auth/settings.rs - Added :

---
+++ src/auth/settings.rs
@@ -0,0 +1,25 @@
+// auth  app settings
+use rocket::Route;
+use super::routes as R;
+
+pub struct Settings {
+    pub routes: Vec<Route>,
+}
+
+impl Settings {
+    pub fn new () -> Self {
+
+        Self {
+            routes: get_routes(),
+        }
+    }
+}
+//  === Routes 
+fn get_routes() -> Vec<Route> {
+    routes![
+        R::hi_json::hi_json,
+        R::hello::hello,
+        R::users::get_all_users,
+        ]
+}
+
