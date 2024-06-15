5ff9b09e836854cf50965dfb573c44b65551b071
SKY <sky012877@gmail.com>
2024-06-14 19:10:18 -0700

    migration/src/field_types.rs - Modified :

--- migration/src/field_types.rs
+++ migration/src/field_types.rs
@@ -1,15 +1,36 @@
 use sea_orm_migration::prelude::*;
 
 
-pub fn id<T: Iden + 'static>(field_enum: T) -> ColumnDef {
+pub fn id <T: Iden + 'static>(field_enum: T) -> ColumnDef {
     // Create and return a new ColumnDef instance in one line.
-    // This assumes that each chained method returns the modified ColumnDef as `&mut ColumnDef`.
-    let mut column_def = ColumnDef::new(field_enum);
-    column_def.integer()
-              .not_null()
-              .auto_increment()
-              .primary_key();
-
-    column_def  // Return the modified ColumnDef
+    ColumnDef::new(field_enum)
+        .integer()
+        .not_null()
+        .auto_increment()
+        .primary_key()
+    .clone()
 }
 
+pub fn name <T: Iden + 'static>(field_enum: T) -> ColumnDef {
+    ColumnDef::new(field_enum)
+        .string()
+        .not_null()
+        .unique_key()
+    .clone()
+}
+
+pub fn email <T: Iden + 'static>(field_enum: T) -> ColumnDef {
+    ColumnDef::new(field_enum)
+        .string()
+        .not_null()
+        .unique_key()
+    .clone()
+}
+
+pub fn current_timestamp <T: Iden + 'static>(field_enum: T) -> ColumnDef {
+    ColumnDef::new(field_enum)
+        .timestamp()
+        .not_null()
+        .default(Expr::current_timestamp())
+    .clone()
+}
