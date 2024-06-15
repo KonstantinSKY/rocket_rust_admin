1decfa5267ac4e6308b3476d277ff16ec12039c8
SKY <sky012877@gmail.com>
2024-06-14 22:48:53 -0700

    src/auth/routes/users.rs - Modified :

--- src/auth/routes/users.rs
+++ src/auth/routes/users.rs
@@ -1,21 +1,10 @@
-use rocket_okapi::openapi;
 use rocket::serde::json::Json;
 use rocket::{get, State};
 use sea_orm::{EntityTrait, DatabaseConnection};
 use super::super::models::user::{Entity as User, Model as UserModel};
-// use sea_orm::EntityTrait;
 
 
-// #[openapi]
 #[get("/auth/users")]
 pub async fn get_all_users(db: &State<DatabaseConnection>) -> Json<Vec<UserModel>> {
-    let users: Vec<UserModel> = User::find().all(db.inner()).await.unwrap_or_else(|_| vec![]);
-    Json(users)
+    Json(User::find().all(db.inner()).await.unwrap_or_default())
 }
-
-// #[openapi]
-// #[get("/users_openapi")]
-// pub async fn get_all_users_openapi(db: &State<DatabaseConnection>) -> Json<Vec<UserModel>> {
-//     let users: Vec<UserModel> = User::find().all(db.inner()).await.unwrap_or_else(|_| vec![]);
-//     Json(users)
-// }
