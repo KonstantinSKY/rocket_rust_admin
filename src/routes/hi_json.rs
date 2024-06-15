952b681f44a3d720284b4c5ae4e5dc67fe56d13d
SKY <sky012877@gmail.com>
2024-06-14 22:48:53 -0700

    src/routes/hi_json.rs - Deleted :

--- src/routes/hi_json.rs
+++
@@ -1,9 +0,0 @@
-use rocket::serde::json::{json, Value, Json};
-use rocket_okapi::openapi;
-
-#[openapi]
-#[get("/hi")]
-pub fn hi_json() -> Json<Value> {
-    Json(json!({"key": "Hello, world! JSON!"}))
-}
-
