9cbb2a09bf9bf62206789ae421ef034a61022197
SKY <sky012877@gmail.com>
2024-06-17 12:07:17 -0700

    src/auth/mod.rs - Modified :

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,3 +2,13 @@ pub mod settings;
 pub mod routes;
 pub mod models;
 pub mod services;
+
+use crate::project::AppSettings;
+
+pub fn get_app_settings() -> AppSettings {
+    AppSettings { 
+        name : settings::NAME, 
+        verbose_name: settings::VERBOSE_NAME,
+        routes: settings::routes(),
+  } 
+}
