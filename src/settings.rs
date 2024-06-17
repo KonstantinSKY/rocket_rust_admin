e46c814e2d0296770d15e0f12c41541b0e329918
SKY <sky012877@gmail.com>
2024-06-17 12:07:17 -0700

    src/settings.rs - Modified :

--- src/settings.rs
+++ src/settings.rs
@@ -1,41 +1,61 @@
 use std::env;
 
+use crate::project::AppSettings;
+
 use dotenv::dotenv;
+
 use rocket::{figment::Figment, Config, Route};
 
-//  === Routes 
-fn get_routes() -> Vec<Route> {
-    let mut routes = Vec::<Route>::new();
-    routes
-        .extend(crate::auth::settings::routes());
-    
-    routes
-    
+// === ADD NEW APP HERE !!!
+// Collect all app setting to one collection
+fn app_setting_collection() -> Vec<AppSettings>{
+    vec![
+        crate::auth::get_app_settings(),
+        // crate::new_app::settings::get(), // Uncomment and edit as needed
+    ]
 }
 
-pub struct Settings {
-    pub secret_key: String,
+// pub struct AppSettings {
+//     pub name: &'static str,
+//     pub verbose_name: &'static str,
+//     pub routes: Vec<Route>,
+// }
+
+//  === Routes 
+fn get_routes(app_settings: &Vec<AppSettings>) -> Vec<Route> {
+    let mut routes = Vec::<Route>::new();
+    for app_settings in app_settings{
+        routes.extend(app_settings.routes.clone());
+    }
+    routes
+}
+
+pub struct ProjectSettings {
+    pub secret_key: String,  
     pub figment: Figment,
+    pub apps_settings: Vec<AppSettings>,
     pub routes: Vec<Route>,
 }
 
-impl Settings {
+impl ProjectSettings {
     pub fn new () -> Self {
         dotenv().ok(); // Load environment variables
+
+        //secret key
         let secret_key = env::var("SECRET_KEY").expect("SECRET_KEY must be set");
         let figment = Config::figment()   
                                 .merge(("secret_key", secret_key.clone()));
         
-        // let apps = Apps::new();
-
+        let apps_settings = app_setting_collection();
+        let routes = get_routes(&apps_settings); 
+        
         Self {
             secret_key,
             figment,
-            routes: get_routes(),
+            apps_settings,
+            routes,
         }        
     }
-    
-    
 }
 
 
