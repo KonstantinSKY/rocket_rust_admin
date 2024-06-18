403c3f8bbd98b1bf63789106caa5e91f6bbc14cc
SKY <sky012877@gmail.com>
2024-06-18 11:05:33 -0700

    src/auth/routes/groups.rs - Modified :

--- src/auth/routes/groups.rs
+++ src/auth/routes/groups.rs
@@ -5,6 +5,7 @@ use rocket::{get, post, State};
 use sea_orm::{entity::*, DatabaseConnection};
 use super::super::models::group;
 use crate::db::{select, insert};
+use crate::project::responses;
 
 #[get("/auth/groups")]
 pub async fn get_all_groups(db: &State<DatabaseConnection>) -> Result<Json<Vec<group::Model>>, rocket::http::Status> {
@@ -53,21 +54,22 @@ pub async fn add_group(db: &State<DatabaseConnection>, new_group: Json<NewGroup>
 
 // Handler to delete a user
 #[delete("/auth/groups/<group_id>")]
-pub async fn delete_group(db: &State<DatabaseConnection>, group_id: i32) -> Result <Status, rocket::http::Status> {
+pub async fn delete_group(db: &State<DatabaseConnection>, group_id: i32) -> Result <Status, Status> {
     let result = group::Entity::delete_by_id(group_id).exec(db.inner()).await;    // Correct
-    
-    match result {
-        Ok(result) => {
-            if result.rows_affected > 0 {
-                Ok(Status::NoContent) // Return 204 No Content if deletion was successful
-            } else {
-                Err(Status::NotFound) // Return 404 Not Found if no rows were affected
-            }
-        }
-        Err(err) => {
-            // println!("Delete error: {:?}", err);
-            Err(Status::InternalServerError) // Return 500 Internal Server Error on failure
-        }
-    }
+    responses::handle_deletion_result(result)
+
+    // match result {
+    //     Ok(result) => {
+    //         if result.rows_affected > 0 {
+    //             Ok(Status::NoContent) // Return 204 No Content if deletion was successful
+    //         } else {
+    //             Err(Status::NotFound) // Return 404 Not Found if no rows were affected
+    //         }
+    //     }
+    //     Err(err) => {
+    //         // println!("Delete error: {:?}", err);
+    //         Err(Status::InternalServerError) // Return 500 Internal Server Error on failure
+    //     }
+    // }
 }
 
