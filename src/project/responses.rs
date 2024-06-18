38be18b385e22fafb774a76a168512d1fce6472b
SKY <sky012877@gmail.com>
2024-06-18 11:57:21 -0700

    src/project/responses.rs - Modified : resutts handler nad prepare rensponses

--- src/project/responses.rs
+++ src/project/responses.rs
@@ -1,4 +1,5 @@
-use rocket::http::Status;
+use rocket::{http::Status, serde::json::Json};
+use sea_orm::DbErr;
 // === Universal Result handlers
 
 
@@ -14,6 +15,65 @@ pub fn handle_deletion_result(result: Result<sea_orm::DeleteResult, sea_orm::DbE
         Err(err) => {
             // println!("Delete error: {:?}", err);
             Err(Status::InternalServerError) // Return 500 Internal Server Error on failure
+        }
+    }
+}
+
+// Generic function to handle the result and convert to JSON response
+pub fn handle_selection_result_by_response_struct<T, R: From<T>>(result: Result<Vec<T>, DbErr>) -> Result<Json<Vec<R>>, Status>  {
+    match result {
+        Ok(models) => {
+            let responses: Vec<R> = models.into_iter().map(R::from).collect();
+            Ok(Json(responses))
+        }
+        Err(_) => {
+            // Log the error internally if needed
+            // error!("Failed to fetch data: {}", err);
+            Err(Status::InternalServerError)
+        }
+    }
+}
+
+
+// Generic function to handle the result and convert to JSON response
+pub fn handle_selection_result<T>(result: Result<Vec<T>, DbErr>) -> Result<Json<Vec<T>>, Status>  {
+    match result {
+        Ok(models) => {
+            Ok(Json(models))
+        }
+        Err(err) => {
+            // Log the error internally if needed
+            // error!("Failed to fetch data: {}", err);
+            Err(Status::InternalServerError)
+        }
+    }
+}
+
+// Generic function to handle the result and convert to JSON response
+pub fn handle_insertion_result<T>(result: Result<T, DbErr>) -> Result<Json<T>, Status>  {
+    match result {
+        Ok(inserted_model) => {
+            Ok(Json(inserted_model))
+        }
+        Err(err) => {
+            // Log the error internally if needed
+            // error!("Failed to fetch data: {}", err);
+            Err(Status::InternalServerError)
+        }
+    }
+}
+
+pub fn handle_insertion_result_by_response_struct<T, R: From<T>>(result: Result<T, DbErr>) -> Result<Json<R>, Status>  {
+    match result {
+        Ok(inserted_model) => {
+            // let response: Vec<R> = models.into_iter().map(R::from).collect();
+            let response = R::from(inserted_model);
+            Ok(Json(response))
+        }
+        Err(_) => {
+            // Log the error internally if needed
+            // error!("Failed to fetch data: {}", err);
+            Err(Status::InternalServerError)
         }
     }
 }
