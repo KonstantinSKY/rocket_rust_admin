34561f66d6e0b473e126aeac093ba06aecfc2def
SKY <sky012877@gmail.com>
2024-06-17 20:19:59 -0700

    src/auth/routes/groups.rs - Modified :

--- src/auth/routes/groups.rs
+++ src/auth/routes/groups.rs
@@ -1,48 +1,56 @@
+use chrono::Utc;
+use rocket::form::name;
+use rocket::http::Status;
 use rocket::serde::{json::Json, Deserialize};
 use rocket::{get, post, State};
 use sea_orm::{entity::*, DatabaseConnection};
-use super::super::models::group::{
-    Entity as Group, 
-    Model as GroupModel,
-    ActiveModel as GroupActiveModel,
-};
-use super::super::database::group as database;
+use super::super::models::group;
+use crate::auth::models::group::ActiveModel;
+use crate::db::insert::insert;
+use crate::db::{select, insert};
+// use super::super::models::group::{GroupModel, GroupActiveModel};
 
 #[get("/auth/groups")]
-pub async fn get_all_groups(db: &State<DatabaseConnection>) -> Json<Vec<GroupModel>> {
-    Json(database::select_all_groups(db).await)
-    // Json(Group::find().all(db.inner()).await.unwrap_or_default())
+pub async fn get_all_groups(db: &State<DatabaseConnection>) -> Result<Json<Vec<group::Model>>, rocket::http::Status> {
+    let result = select::select_all::<group::Entity>(db).await;
+    
+    match result {
+        Ok(models) => Ok(Json(models)),
+        Err(err) => { // Log the error or handle it as needed
+              // Log the error internally
+            //   error!("Failed to fetch groups: {}", err);
+              Err(Status::InternalServerError) // Respond with only the HTTP status code
+        }
+    }
 }
 
 // Post New group
 #[derive(Deserialize)]
 pub struct NewGroup {
     pub name: String,
-    pub description: String,
+    pub description: Option<String>,
 }
+
 // Handler to add a new group
 #[post("/auth/groups", data = "<new_group>")]
-pub async fn add_group(db: &State<DatabaseConnection>, new_group: Json<NewGroup>) -> Json<GroupModel> {
-    let group = GroupActiveModel {
+pub async fn add_group(db: &State<DatabaseConnection>, new_group: Json<NewGroup>) -> Result<Json<group::Model>, rocket::http::Status> {
+    
+    let active_group = group::ActiveModel {
         name: Set(new_group.name.clone()),
-        description: Set(Some(new_group.description.clone())),
-        created_at: Set(chrono::Utc::now().naive_utc()),  // Assuming created_at is automatically set
+        description: Set(new_group.description.clone()),
+        created_at: Set(Utc::now().naive_utc()),
         ..Default::default()
     };
-    println!("New group: {:#?}", group);
-    let insert_result = Group::insert(group)
-        .exec(db.inner())
-        .await
-        .expect("Failed to insert new group");
 
-    
-     // Fetch the newly created group using the last inserted ID
-     let new_group = Group::find_by_id(insert_result.last_insert_id)
-    .one(db.inner())
-    .await
-    .expect("Failed to retrieve new group")
-    .expect("New group not found");
-    println!("New group: {:#?}", new_group);
-    Json(new_group) 
-    
+    let insert_result = insert::insert::<group::Entity, _>(db, active_group).await;
+
+    match insert_result {
+        Ok(inserted_model) => {
+            Ok(Json(inserted_model))
+        },
+        Err(error) => {
+            println!("Insert error: {:?}", error);
+            Err(rocket::http::Status::InternalServerError)
+        }
+    }
 }
