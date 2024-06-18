4fd3b706ff63dbd1182d9f8dcdfe84fc4ef691e4
SKY <sky012877@gmail.com>
2024-06-17 20:19:59 -0700

    src/auth/routes/users.rs - Modified :

--- src/auth/routes/users.rs
+++ src/auth/routes/users.rs
@@ -2,9 +2,21 @@ use rocket::serde::json::Json;
 use rocket::{get, State};
 use sea_orm::{EntityTrait, DatabaseConnection};
 use super::super::models::user::{Entity as User, Model as UserModel};
+use rocket::http::Status;
+use super::super::models::user;
+use crate::db::select;
+
 
 
 #[get("/auth/users")]
-pub async fn get_all_users(db: &State<DatabaseConnection>) -> Json<Vec<UserModel>> {
-    Json(User::find().all(db.inner()).await.unwrap_or_default())
+pub async fn get_all_users(db: &State<DatabaseConnection>) -> Result<Json<Vec<user::Model>>, rocket::http::Status> {
+    let result = select::select_all::<user::Entity>(db).await;
+    
+    match result {
+        Ok(models) => Ok(Json(models)),
+        Err(err) => {                                        // Log the error or handle it as needed
+            //   error!("Failed to fetch groups: {}", err)          // Log the error internally
+             Err(Status::InternalServerError)                       // Respond with only the HTTP status code
+        }
+    }
 }
